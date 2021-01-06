99f621936443eb8c43ce923286af67e6a6e57a5f
Johnathon Selstad <makeshifted@gmail.com>
2021-01-05 21:19:03 -0800

    Make archiving threads work...
    Soon to allow for renaming threads...

--- threadweaver/threadweaver.py
+++ threadweaver/threadweaver.py
@@ -1,8 +1,10 @@
+from asyncio.tasks import sleep
 from redbot.core import commands, Config
 import discord
 from   discord import Embed, Member, Message, RawReactionActionEvent, Client, Guild, TextChannel, CategoryChannel
 from   discord.ext.commands import Cog
 from datetime import datetime, timedelta
+import re
 
 class Threadweaver(commands.Cog):
     """Threadweaver creates temporary channels based on emoji :thread: reactions."""
@@ -79,9 +81,43 @@ class Threadweaver(commands.Cog):
         embed.set_footer(text=f'View all settings with "/threadweaver-settings"')
         await ctx.send(embed=embed) 
 
-    async def make_channel(self, name, guild):
+    async def make_channel_friendly(self, name : str, guild : Guild):
         sep = await self.config.guild(guild).name_separator()
         return name.replace(" ", sep).lower()
+        
+    def get_thread_owner(self, guild : Guild, thread : TextChannel) -> (Member) :
+        thread_topic : str = thread.topic
+        if(thread_topic.startswith("Thread by ")):
+            match  : re.Match = re.search(r"<@[0-9]*?>", thread_topic)
+            owner_id = int(match.group()[2:-1])
+            member : Member   = discord.utils.get(guild.members, id=owner_id)
+            return member
+        else:
+            return None
+
+    async def archive_thread(self, channel : TextChannel):
+        # Read the first 5000 messages of the thread and mirror them to the thread archive...
+        full_history : list[Message] = await channel.history(limit=5000, oldest_first=True).flatten()
+        for thread_message in full_history:
+            embed=discord.Embed(description=thread_message.content, color=0xff4500)
+            embed.set_author(name=thread_message.author.display_name, icon_url=thread_message.author.avatar_url)
+            await self.thread_archive_channel.send(embed=embed)
+            await sleep(0.01)
+
+        # Delete the thread when we're done
+        await channel.delete(reason="Archived Old Thread in Thread Archive; Deleting Thread")
+
+    @commands.command(name="archive-thread",
+                      description="This command archives all the thread's messages to thread-archive and deletes the thread.")
+    @commands.guild_only()
+    async def archive_thread_command(self, ctx : Message):
+        await self.verify_server_structure(ctx.guild)
+        thread_owner : Member = self.get_thread_owner(ctx.guild, ctx.channel)
+        if thread_owner is not None:
+            if(ctx.author.id == thread_owner.id):
+                await self.archive_thread(ctx.channel)
+            else:
+                await ctx.send("Only the thread owner <@"+thread_owner.id+"> may archive this thread.")
 
     async def verify_server_structure(self, guild: Guild):
         """
@@ -107,7 +143,7 @@ class Threadweaver(commands.Cog):
                 print("[THREADWEAVER] ERROR: Insufficient permissions to create Categories!  Please give me more permissions!")
 
         # Create the "Thread Archive" Channel if it doesn't exist
-        thread_archive_name = await self.make_channel(await self.config.guild(guild).thread_archive_name(), guild)
+        thread_archive_name = await self.make_channel_friendly(await self.config.guild(guild).thread_archive_name(), guild)
         for channel in self.bot.get_all_channels():
             if str(channel) == thread_archive_name:
                 self.thread_archive_channel : TextChannel = channel
@@ -121,22 +157,15 @@ class Threadweaver(commands.Cog):
 
         # Iterate through all the existing threads, checking for the age of the latest message
         interval_days = await self.config.guild(guild).prune_interval_days()
-        thread_prefix = await self.make_channel(await self.config.guild(guild).thread_prefix() + 
+        thread_prefix = await self.make_channel_friendly(await self.config.guild(guild).thread_prefix() + 
                                                 await self.config.guild(guild).name_separator(), guild)
         for channel in self.bot.get_all_channels():
             channel : TextChannel = channel
             if str(channel) != thread_archive_name and str(channel).startswith(thread_prefix):
                 latest_message : list[Message] = await channel.history(limit=1).flatten()
+                # If the latest message is older than the time interval, archive the thread
                 if len(latest_message) > 0 and latest_message[0].created_at < datetime.now() - timedelta(days=interval_days):
-                    # Read the first 5000 messages of the thread and mirror them to the thread archive...
-                    full_history : list[Message] = await channel.history(limit=5000, oldest_first=True).flatten()
-                    for thread_message in full_history:
-                        embed=discord.Embed(description=thread_message.content, color=0xff4500)
-                        embed.set_author(name=thread_message.author.display_name, icon_url=thread_message.author.avatar_url)
-                        await self.thread_archive_channel.send(embed=embed)
-
-                    # Delete the thread when we're done
-                    channel.delete(reason="Archived Old Thread in Thread Archive; Deleting Thread")
+                    self.archive_thread(channel)
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload: RawReactionActionEvent) -> None:
@@ -157,7 +186,7 @@ class Threadweaver(commands.Cog):
                 await self.verify_server_structure(guild)
 
                 thread_channel = None
-                thread_name    = await self.make_channel(await self.config.guild(guild).thread_prefix() + " " + 
+                thread_name    = await self.make_channel_friendly(await self.config.guild(guild).thread_prefix() + " " + 
                                         str(message.author.name) + " " + str(message.id)[-4:], guild)
 
                 # Add the user to the thread if it already exists
@@ -185,7 +214,7 @@ class Threadweaver(commands.Cog):
                         message.author     : discord.PermissionOverwrite(read_messages=True)
                     }
                     thread_channel : TextChannel = await guild.create_text_channel(
-                        thread_name, overwrites=overwrites, topic="\n\nDiscussion Thread: \n"+message.content, category=self.thread_category,
+                        thread_name, overwrites=overwrites, topic="Thread by <@" + str(message.author.id) +">: \n"+message.content, category=self.thread_category,
                         reason = member.display_name + " added a :thread: emoji to " + message.author.display_name + "'s message.")
                     print("[THREADWEAVER] "+member.display_name + " created a new thread: #" + thread_name + " from this message: \n"+message.jump_url)
 
@@ -211,7 +240,7 @@ class Threadweaver(commands.Cog):
                 member  : Member  = discord.utils.get(guild.members, id=payload.user_id)
 
                 thread_channel = None
-                thread_name    = await self.make_channel(await self.config.guild(guild).thread_prefix() + " " + 
+                thread_name    = await self.make_channel_friendly(await self.config.guild(guild).thread_prefix() + " " + 
                                         str(message.author.name) + " " + str(message.id)[-4:], guild)
 
                 # Remove the user from the thread
